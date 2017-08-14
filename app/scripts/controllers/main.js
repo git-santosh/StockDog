78b174a2e78dc3c6a39289c0d04143fc3270b4c6
santosh suryawanshi <santosh.suryawanshi28@gmail.com>
2017-08-14 18:02:21 +0530

    Scope inheritance & Event Transmission example

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,10 +9,14 @@
  */
 angular.module('stockDogApp')
   .controller('MainCtrl', function ($scope, $location, WatchlistService) {
+   $scope.ans =42;
+   $scope.$on('ping',function(){
+           console.log('pong');
+   })
    $scope.watchlists = WatchlistService.query();
    $scope.$watch(function () {
         return $location.path();
-      }, function (path) {
+      }, function (path)        {
       if (_.includes(path, 'watchlist')) {
               $scope.activeView = 'watchlist';
       } else {
@@ -20,3 +24,7 @@ angular.module('stockDogApp')
       }
    });
   });
+angular.module('stockDogApp').controller('myCtrl',function($scope){
+        console.log($scope.ans);
+        $scope.$emit('ping');
+})
